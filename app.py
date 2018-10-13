1ce37feb37ae192e8846675d5408b7b772361630
Greg Cordover <ghc@ixchi.me>
2018-10-12 22:51:19 -0500

    Add basic login features.

--- app.py
+++ app.py
@@ -1,11 +1,85 @@
-from flask import Flask
+from flask import Flask, Response, abort, request, jsonify, redirect, url_for
+from flask_login import login_user, current_user, logout_user, login_required
+
+from db import db
+from login import login_manager
+import models
 
 app = Flask(__name__)
+app.config.from_envvar('ROBIN_CONFIG')
+
+db.init_app(app)
+login_manager.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+@login_manager.user_loader
+def user_loader(user_id):
+    return models.User.query.get(user_id)
 
 
 @app.route('/')
 def home():
+    if current_user.is_authenticated:
+        return 'Hello, %s.' % current_user.name
+
     return 'Hello, world!'
+
+
+@app.route('/auth/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = models.User.query.filter_by(email=email).first()
+        print(user)
+        if not user:
+            return abort(401)
+
+        if not user.check_password(password):
+            return abort(401)
+
+        login_user(user)
+
+        return redirect(url_for('home'))
+    else:
+        return Response('''
+        <form action="" method="post">
+            <p><input type=text name=email>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+        ''')
+
+
+@app.route('/auth/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/auth/register', methods=['POST'])
+def register():
+    user = models.User(
+        name=request.form.get('name'),
+        email=request.form.get('email'),
+        password=request.form.get('password'),
+    )
+    db.session.add(user)
+    db.session.commit()
+
+    return 'Added user.'
+
+
+@app.route('/auth/_users')
+def hidden_users():
+    users = models.User.query.all()
+
+    return jsonify(list(map(lambda user: {'id': user.id, 'email': user.email}, users)))
 
 
 if __name__ == '__main__':
---
+++ models/user.py
@@ -0,0 +1,29 @@
+import bcrypt
+
+from db import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    def __init__(self, name: str, email: str, password: str):
+        self.name = name
+        self.email = email
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    def check_password(self, compare_to) -> bool:
+        return bcrypt.checkpw(compare_to.encode('utf-8'), self.password.encode('utf-8'))
+
+    def is_active(self) -> bool:
+        return True
+
+    def get_id(self) -> int:
+        return self.id
+
+    def is_authenticated(self) -> bool:
+        return True
+
+    def is_anonymous(self) -> bool:
+        return False
