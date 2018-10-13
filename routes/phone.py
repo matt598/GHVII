cb8017b11e2af5a2ae6a949b9417e6791ee21ad0
Greg Cordover <ghc@ixchi.me>
2018-10-13 11:35:02 -0500

    Try to resolve country selection.

--- routes/phone.py
+++ routes/phone.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, request, url_for
 from tropo import Tropo, Result
 
+import data
+
 
 phone_app = Blueprint('phone', __name__, url_prefix='/phone')
 
@@ -24,8 +26,27 @@ def country():
 
     answer = r.getInterpretation()
 
+    countries = data.country_for_code(answer)
+
     t = Tropo()
-    t.say('You entered')
-    t.say(answer, _as='DIGITS')
+
+    if not countries:
+        t.ask(
+            choices='[1-3 DIGITS]',
+            name='code',
+            say='Invalid country code, please try again.',
+            timeout=5,
+        )
+        t.on(event='continue', next=url_for('phone.country'))
+    elif len(countries) == 1:
+        t.say('You selected')
+        t.say(countries[0]['name'])
+    else:
+        t.ask(
+            choices=', '.join(list(map(lambda country: country['name'], countries))),
+            name='country',
+            say='Please select one of the following countries.',
+            timeout=5,
+        )
 
     return t.RenderJson()
