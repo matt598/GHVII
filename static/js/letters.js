6cb41586a9de3cbb33387bbff10b7ec67d399a86
Greg Cordover <ghc@ixchi.me>
2018-10-14 00:28:08 -0500

    beautiful

--- static/js/letters.js
+++ static/js/letters.js
@@ -34,3 +34,54 @@ function applySuggested(str, choice) {
     let newStr = str.slice(0, -1);
     return newStr + choice;
 }
+
+class LetterSuggestor {
+    constructor(inputSelector) {
+        this.helper = document.getElementById('input-helper');
+        this.names = Array.from(document.querySelectorAll(inputSelector));
+        this.names.forEach(name => {
+            name.addEventListener('keyup', this.onKeyUp.bind(this));
+        });
+        Array.from(document.querySelectorAll('input,textarea')).forEach(input => {
+            input.addEventListener('focus', this.onFocus.bind(this));
+        });
+    }
+
+    onKeyUp(ev) {
+        const suggested = getSuggestedForInput(ev.target.value);
+        const container = this.helper.querySelector('div');
+
+        if (!suggested) {
+            container.innerHTML = 'No suggestions';
+            return;
+        }
+
+        container.innerHTML = '';
+        suggested.map(suggestion => {
+            const btn = document.createElement('a');
+            btn.href = '#';
+            btn.innerHTML = suggestion;
+            btn.classList.add('btn', 'btn-sm', 'btn-secondary', 'mr-1');
+            btn.addEventListener('click', () => {
+                ev.target.value = applySuggested(ev.target.value, suggestion);
+                this.onKeyUp(ev);
+                ev.target.focus();
+            });
+            return btn;
+        }).forEach(btn => {
+            container.appendChild(btn);
+        });
+    }
+
+    onFocus(ev) {
+        let found = false;
+
+        this.names.forEach(name => {
+            if (name.id == ev.target.id) {
+                found = true;
+            }
+        });
+
+        this.helper.style.display = (found) ? 'block': 'none';
+    }
+}
